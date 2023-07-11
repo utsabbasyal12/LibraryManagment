ea6a3519cb8d1dca71ebeb7588279fe4dabbb7b9
Utsav Basyal <utsabbasyal12@gmail.com>
2023-07-11 22:31:49 +0545

    Books Index Page Created

---
+++ LibraryManagment/Controllers/BookController.cs
@@ -0,0 +1,40 @@
+﻿using LibraryManagment.Data;
+using LibraryManagment.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace LibraryManagment.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BookController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var books = _context.Books.ToList();
+            List<BookViewModel> list = new List<BookViewModel>();
+            if (books != null)
+            {
+                foreach (var book in books)
+                {
+                    var BookViewModel = new BookViewModel()
+                    {
+                        BookId = book.BookId,
+                        Title = book.Title,
+                        Author = book.Author,
+                        Genre = book.Genre,
+                        AvailabilityStatus = book.AvailabilityStatus,
+                    };
+                    list.Add(BookViewModel);
+                }
+                return View(list);
+            }
+            return View(list);
+        }
+    }
+}
--- LibraryManagment/ViewModels/BookViewModel.cs
+++ LibraryManagment/ViewModels/BookViewModel.cs
@@ -1,16 +1,14 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel;
 
-namespace LibraryManagment.Models
+namespace LibraryManagment.ViewModels
 {
-    [Table("tblBooks")]
-    public class BooksModel
+    public class BookViewModel
     {
-        [Key]
         public int BookId { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Author { get; set; } = string.Empty;
         public string Genre { get; set; } = string.Empty;
+        [DisplayName("Status")]
         public int AvailabilityStatus { get; set; }
     }
 }
