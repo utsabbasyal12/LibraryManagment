fbb702727e5c7cb128cb37de8a000e069a222993
Utsav Basyal <utsabbasyal12@gmail.com>
2023-07-12 00:23:47 +0545

    Added enum dropdown and display status in text value

--- LibraryManagment/ViewModels/BookViewModel.cs
+++ LibraryManagment/ViewModels/BookViewModel.cs
@@ -1,14 +1,22 @@
 ﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
 
 namespace LibraryManagment.ViewModels
 {
     public class BookViewModel
     {
         public int BookId { get; set; }
-        public string Title { get; set; } = string.Empty;
+        [Required(ErrorMessage ="Requried!")]
+        public string Title { get; set; }
         public string Author { get; set; } = string.Empty;
         public string Genre { get; set; } = string.Empty;
         [DisplayName("Status")]
         public int AvailabilityStatus { get; set; }
+    }
+    public enum Status
+    {
+        Available = 1,
+        Reserved = 2
     }
 }
