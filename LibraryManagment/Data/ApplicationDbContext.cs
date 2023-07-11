22665f04446106ef1c92822eccaa087877708015
Utsav Basyal <utsabbasyal12@gmail.com>
2023-07-11 21:10:20 +0545

    Add project files.

---
+++ LibraryManagment/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using LibraryManagment.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagment.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BooksModel> Books { get; set; }
+    }
+}
---
+++ LibraryManagment/Models/BooksModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LibraryManagment.Models
+{
+    [Table("tblBooks")]
+    public class BooksModel
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public string Genre { get; set; } = string.Empty;
+        public int AvailabilityStatus { get; set; }
+    }
+}
