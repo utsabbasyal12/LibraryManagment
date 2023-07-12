de87cdb90f82cf8d7feb2bee1260b76a1a94b454
Utsav Basyal <utsabbasyal12@gmail.com>
2023-07-12 12:08:29 +0545

    Create BookManagmentEndpoint.cs

---
+++ LibraryManagment/EndPoints/BookManagmentEndpoint.cs
@@ -0,0 +1,12 @@
+﻿namespace LibraryManagment.EndPoints
+{
+    public class BookManagmentEndpoint
+    {
+        public static string GetAll = "BookManagementAPI/BookAPI/GetAllBooks";
+        public static string GetById = "BookManagementAPI/BookAPI/GetBookById";
+        public static string Add = "BookManagementAPI/BookAPI/AddBook";
+        public static string Update = "BookManagementAPI/BookAPI/UpdateBook";
+        public static string Delete = "BookManagementAPI/BookAPI/DeleteBook";
+
+    }
+}
