4de18fe0d1dc630ac21219a918d112351cf09692
Utsav Basyal <utsabbasyal12@gmail.com>
2023-07-12 21:31:22 +0545

    Create LibraryAPIController.cs

---
+++ BookManagementAPI/Controllers/LibraryAPIController.cs
@@ -0,0 +1,49 @@
+﻿using BookManagementAPI.Models;
+using BookManagementAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookManagementAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LibraryAPIController : ControllerBase
+    {
+        private readonly ILibraryMemberService _service;
+
+        public LibraryAPIController(ILibraryMemberService service)
+        {
+            _service = service;
+        }
+        [HttpGet("GetAllMembers")]
+        public async Task<IActionResult> GetAllMembers()
+        {
+            var members = await _service.GetAllMembers();
+            return Ok(members);
+        }
+        [HttpGet("GetMemberById")]
+        public async Task<IActionResult> GetMemberById(int MemberId)
+        {
+            var member = await _service.GetMemberByIdAsync(MemberId);
+            return Ok(member);
+        }
+        [HttpPost("AddMember")]
+        public async Task<IActionResult> AddMembers([FromBody] LibraryMembersModel model)
+        {
+            var data = await _service.AddMembersAsync(model);
+            return Ok(data);
+        }
+        [HttpPost("UpdateMember")]
+        public async Task<IActionResult> UpdateMembers([FromBody] LibraryMembersModel model)
+        {
+            var data = await _service.UpdateMembersAsync(model);
+            return Ok(data);
+        }
+        [HttpGet("DeleteMember")]
+        public async Task<IActionResult> DeleteMember(int Id)
+        {
+            await _service.DeleteMemberAsync(Id);
+            return Ok("Member Deleted Successfully.");
+        }
+    }
+}
